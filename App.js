d78bcf85eb769869cff4689dbda75d2fb9253bff
Youssef El habchi <ellhydra1@gmail.com>
2020-12-04 13:01:40 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Scanning For Viruses...</Text>
+<ActivityIndicator 
+  size = "large" 
+  color = "white"
+/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#623FA0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+    fontSize: 20,
+    margin: 20
+  }
 });
